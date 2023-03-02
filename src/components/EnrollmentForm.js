422ddd32f925d97c9bf3d703179ac7766987a9b4
limseungchae <tmdco12@naver.com>
2023-03-02 17:11:06 +0900

    대학생 등록 폼 작성 - state를 이용한 환영메세지 출력

---
+++ src/components/EnrollmentForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import '../App.css';
+
+const EnrollmentForm = () => {
+    const  [firstName, setFirsName] = useState("");
+    const  [lastName, setLastName] = useState("");
+    const  [welcomMessage, setwelcomMessage] = useState("");
+    const handleSubmit = (e) => {
+        setwelcomMessage(`환영합니다. ${firstName} ${lastName}`);
+        e.preventDefault(); // submit 기능 중지
+    };
+    return(
+        <div>
+        <form className="enrolForm" onSubmit={handleSubmit}>
+        <h1>대학생 등록양식</h1>
+        <div><label>First Name</label>
+            <input type="text" name="fname"
+            onBlur={(e) =>
+                setFirsName(e.target.value)} /></div>
+
+        <div><label>Last Name</label>
+            <input type="text" name="lname"
+            onBlur={(e) =>
+                setLastName(e.target.value)}/></div>
+
+            <div><button type="submit">등록하기</button></div>
+        </form>
+
+            <label id="studentMsg" className="message">
+                {welcomMessage}
+            </label>
+        </div>
+    );
+};
+
+export default EnrollmentForm;
