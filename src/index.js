84ac11594b18a2c6aa2cceafcb87a5cd68038ebb
limseungchae <tmdco12@naver.com>
2023-03-02 16:13:40 +0900

    React App 클릭이벤트 테스트

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App.js';
 
 ReactDom.render(
     <React.StrictMode>
         <h1>Just React</h1>
     </React.StrictMode>,
     document.querySelector('#app')
+);
+
+ReactDom.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.querySelector('#root')
 );
